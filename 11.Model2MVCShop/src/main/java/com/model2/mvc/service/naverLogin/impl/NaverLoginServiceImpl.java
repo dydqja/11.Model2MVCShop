295250f7e5486b224225cf2bdb5b243d959cb660
DESKTOP-F1M4T0M\LG <dydqja9311@gmail.com>
2023-05-02 15:36:48 +0900

    test/dev: NaverLoginAPI 토큰발행 후 사용자정보 가져오기 완료 / 사용자정보 DB 연동시켜야함(기존 User domain 및 DBtable 수정해야함 or 따로만들기) / 23.05.02

--- 11.Model2MVCShop/src/main/java/com/model2/mvc/service/naverLogin/impl/NaverLoginServiceImpl.java
+++ 11.Model2MVCShop/src/main/java/com/model2/mvc/service/naverLogin/impl/NaverLoginServiceImpl.java
@@ -35,10 +35,7 @@ public class NaverLoginServiceImpl implements NaverLoginService {
 
     // 로그인성공시 리턴될 URL
 	@Value("#{applicationProperties['naver.redirect.url']}")
-    private String REDIRECT_URI;
-    
-    // 세션 유효값검증용 체크용
-    private static final String SESSION_STATE = "oauth_state"; 
+    private String REDIRECT_URI;     
 
     // 프로필 조회 API URL
     private final static String PROFILE_API_URL = "https://openapi.naver.com/v1/nid/me";
@@ -46,32 +43,36 @@ public class NaverLoginServiceImpl implements NaverLoginService {
     @Override
     public String getAuthorizationUrl(HttpSession session) {
     	
-    	System.out.println("NaverLoginServiceImpl에서 getAuthorizationUrl 실행됨.");
+    	System.out.println("NaverLoginServiceImpl에서 getAuthorizationUrl 실행됨.");    	
     	
         // 세션 유효성 검증을 위하여 난수를 생성
         String state = generateRandomString();
-        // 생성한 난수 값을 session에 저장
-        setSession(session, state);
-
+        String sessionId = session.getId();
+        // 생성한 난수 값을 session에 저장        
+        session.setAttribute("oauth_state", state);
+        
+        System.out.println("난수값(state) == 저장된 세션값(session) 이어야 함.");
+        System.out.println("state ["+state+"]"+"== session ["+session.getAttribute("oauth_state")+"]");        
+       
         // Scribe에서 제공하는 인증 URL 생성 기능을 이용하여 네아로 인증 URL 생성
         OAuth20Service oauthService = new ServiceBuilder()
         		.apiKey(CLIENT_ID)
                 .apiSecret(CLIENT_SECRET)
                 .callback(REDIRECT_URI)
-                .state(state) //앞서 생성한 난수값을 인증 URL생성시 사용함
-                .build(NaverLogin.instance());
+                .state(state) //앞서 생성한 난수값을 인증 URL생성시 사용함                
+                .build(NaverLogin.instance());            
+        
         return oauthService.getAuthorizationUrl();
     }
     
     @Override
     public OAuth2AccessToken getAccessToken(HttpSession session, String code, String state) throws IOException {
     	
-    	System.out.println("NaverLoginServiceImpl 에서 getAccessToken 실행됨.");
+    	System.out.println("NaverLoginServiceImpl 에서 getAccessToken 실행됨.");    	
+    	System.out.println("'state' 값 ["+state+"] 과 'oauth_state' 값 ["+session.getAttribute("oauth_state")+"] 이 일치해야함");
     	
         // Callback으로 전달받은 세선검증용 난수값과 세션에 저장되어있는 값이 일치하는지 확인
-        String sessionState = getSession(session);
-        
-        System.out.println("getSession(session) == "+sessionState);
+        String sessionState = (String) session.getAttribute("oauth_state");        
         
         if (StringUtils.pathEquals(sessionState, state)) {
 
@@ -86,27 +87,20 @@ public class NaverLoginServiceImpl implements NaverLoginService {
             OAuth2AccessToken accessToken = oauthService.getAccessToken(code);
             return accessToken;
         }
+        System.out.println("세션검증실패");
         return null;
     }
 
     // 세션 유효성 검증을 위한 난수 생성기
     private String generateRandomString() {
         return UUID.randomUUID().toString();
-    }
-
-    // http session에 데이터 저장
-    private void setSession(HttpSession session, String state) {
-        session.setAttribute(SESSION_STATE, state);
-    }
-
-    // http session에서 데이터 가져오기
-    private String getSession(HttpSession session) {
-        return (String) session.getAttribute(SESSION_STATE);
-    }
+    }   
 
     // Access Token을 이용하여 네이버 사용자 프로필 API를 호출
     @Override
     public String getUserProfile(HttpSession session, OAuth2AccessToken oauthToken) throws IOException {
+    	
+    	System.out.println("NaverLoginServiceImpl 에서 getUserProfile 실행됨.");
 
         OAuth20Service oauthService = new ServiceBuilder()
                 .apiKey(CLIENT_ID)
--- 11.Model2MVCShop/src/main/java/com/model2/mvc/web/naverLogin/NaverLoginRestController.java
+++ 11.Model2MVCShop/src/main/java/com/model2/mvc/web/naverLogin/NaverLoginRestController.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.json.simple.parser.JSONParser;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
@@ -17,8 +17,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.scribejava.core.model.OAuth2AccessToken;
-import com.google.gson.Gson;
+
 import com.model2.mvc.service.naverLogin.NaverLoginService;
+
 
 
 
@@ -40,11 +41,14 @@ public class NaverLoginRestController {
     @RequestMapping( value="/getNaverAuthUrl", method=RequestMethod.GET )
     public @ResponseBody String getNaverAuthUrl( HttpSession session) throws Exception {
     	
-    	System.out.println("/naverLogin/getNaverAuthUrl :: GET");
+    	System.out.println("/naverLogin/getNaverAuthUrl :: GET");    	
     	
     	String reqUrl = naverLoginService.getAuthorizationUrl(session);
     	
-    	System.out.println("NaverLoginServiceImpl에서 return된 accessToken 값은: ["+reqUrl+"]");
+//    	System.out.println(session.getAttribute("oauth_state"));
+    	System.out.println("1.getNaverAuthUrl에서의 session Id = " +session.getId());
+    	
+    	System.out.println("getNaverAuthUrl에서 return된 Url 값은: ["+reqUrl+"]");    	
     	
         return reqUrl;
     }
@@ -52,54 +56,67 @@ public class NaverLoginRestController {
     @RequestMapping(value = "/oauth_naver")
     public String oauthNaver(HttpServletRequest request, HttpServletResponse response) throws Exception {
     	
-    	System.out.println("/naverLogin/oauth_naver :: GET");        
-
-//        JSONParser parser = new JSONParser(); // => JSON객체 <-> java객체 변환에 사용
-//        Gson gson = new Gson(); // JSON데이터와 자바객체간의 직렬화 및 역직렬화 처리        
-
-        HttpSession session = request.getSession();
+    	System.out.println("/naverLogin/oauth_naver :: GET");            	
+    	
+    	HttpSession session = request.getSession();
+    	System.out.println("2.oauthNaver에서의 session Id =" + session.getId());
+    	
         String code = request.getParameter("code");
         String state = request.getParameter("state");
         String error = request.getParameter("error");
         
-        System.out.println("QueryString으로 온 session,code,state,error값은? => session:["+session+"], code:["+code+"], state:["+state+"], error:["+error+"]");
+        System.out.println("QueryString으로 온 code,state,error값은? => code:["+code+"], state:["+state+"], error:["+error+"]");
         
         // 로그인 팝업창에서 취소버튼 눌렀을경우
         if ( error != null ){
             if(error.equals("access_denied")){
                 return "redirect:/login";
             }
-        }    
+        }            
 
         OAuth2AccessToken oauthToken;
         oauthToken = naverLoginService.getAccessToken(session, code, state);
         
-        System.out.println("NaverLoginServiceImpl에서 return된 aouthToken 값은: ["+oauthToken+"]");
+        System.out.println("getAccessToken에서 return된 aouthToken 값은: ["+oauthToken+"]");        
+    
+        //로그인 사용자 정보를 읽어온다.
+        String loginInfo = naverLoginService.getUserProfile(session, oauthToken);
         
+        System.out.println("getUserProfile에서 return된 response.getBody() 값은: ["+loginInfo+"]");
+        
+        JSONObject obj = null;
+                
+        // JSON 형태로 변환
+        obj = new JSONObject(loginInfo);
+        System.out.println("JSON형식의 문자열 -> JSONObject:["+obj+"]로 변환");        
+        
+        JSONObject callbackResponse = null;
+        callbackResponse = obj.getJSONObject("response");
+        System.out.println("JSONObject 에서 response값만 빼옴 ==> ["+callbackResponse+"]");
+        
+        String snsId = null;
+        snsId = callbackResponse.getString("id");
+        System.out.println("response값에서 id값 가져옴 ==> ["+snsId+"]");
+
+        if (snsId != null && !snsId.equals("")) {
+        	
+//        	NaverLoginUser naverLoginUser = new NaverLoginUser()
+//        			.setSnsId()
+//        			.set
+        	
+
+            /** 
+            
+                TO DO : 리턴받은 naverUniqueNo 해당하는 회원정보 조회 후 로그인 처리 후 메인으로 이동
+            
+            */
+
+        // 네이버 정보조회 실패
+        } else {            
+            throw new Exception("Error occurred");
+        }
         return null;
+        
     }
-//        //로그인 사용자 정보를 읽어온다.
-//        String loginInfo = naverLoginService.getUserProfile(session, oauthToken);
-//
-//        // JSON 형태로 변환
-//        Object obj = parser.parse(loginInfo);
-//        JSONObject jsonObj = JSONObject.fromObject(gson.toJson(obj));
-//        JSONObject callbackResponse = (JSONObject) jsonObj.get("response");
-//        String naverUniqueNo = callbackResponse.get("id").toString();
-//
-//        if (naverUniqueNo != null && !naverUniqueNo.equals("")) {
-//
-//            /** 
-//            
-//                TO DO : 리턴받은 naverUniqueNo 해당하는 회원정보 조회 후 로그인 처리 후 메인으로 이동
-//            
-//            */
-//
-//        // 네이버 정보조회 실패
-//        } else {
-//            throw new ErrorMessage("네이버 정보조회에 실패했습니다.");
-//        }
-//
-//    }
-//	
+	
 }
